15b2e10b5377d2d7e26126c7d1cf44d3d747fdea
danish khan <danish2019@namal.edu.pk>
2021-10-02 23:20:05 +0500

    Version 2

---
+++ v02.py
@@ -0,0 +1,11 @@
+# calculator in Python(V0.2) by Danish Khan 
+
+def Sub(num1, num2):
+	return num1 - num2
+
+# Sqr root by jawad(V01)
+def SqRoot(num):
+	return num ** (1/2)
+# main
+
+print(Sub(5, 3),SqRoot(25))

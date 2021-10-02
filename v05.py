268b1f55083aaad1b785cd3cf8256b9ffd257651
MIAN ZUBAIR <zubair6986597@gmail.com>
2021-10-02 23:06:57 +0500

    version 5

---
+++ v05.py
@@ -0,0 +1,17 @@
+# calculator in  Python(v0.5) by Zubair
+
+# Mul function
+def mul(num1, num2):
+	return num1 * num2
+# main
+res = mul(5, 3)
+print(res)
+
+
+# Square Root Function
+def SqRoot(num):
+	return num ** (1/2)
+
+# Main
+if __name__ == "__main__":
+	print(SqRoot(25))

5e7779fa44d518dfcd236781b7a8c8f0ed14506d
MuhammadAnas789 <manas2019@namal.edu.pk>
2021-10-03 00:23:09 +0500

    V03 By Anas

---
+++ v03.py
@@ -0,0 +1,18 @@
+# calculator in  Python(v0.3)
+
+# Square Root Function
+def SqRoot(num):
+	return num ** (1/2)
+
+# Main
+if __name__ == "__main__":
+	print(SqRoot(25))
+
+
+
+def div (num1,num2):
+       return num1/num2
+
+#main
+
+print(div(15,3))

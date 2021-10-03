02db29f1701d43509e136fd6fb7d7116c06b2b05
Sharjeel-Akram <sharjeel.akram0200@gmail.com>
2021-10-03 12:24:16 +0500

    add function by sharjeel

---
+++ v02.py
@@ -0,0 +1,12 @@
+# calculator in  Python(v0.1)
+
+# Square Root Function
+def SqRoot(num):
+	return num ** (1/2)
+def add(num1,num2):
+	return num1 + num2
+
+# Main
+if __name__ == "__main__":
+	print(SqRoot(25))
+	print(add(5,3))
